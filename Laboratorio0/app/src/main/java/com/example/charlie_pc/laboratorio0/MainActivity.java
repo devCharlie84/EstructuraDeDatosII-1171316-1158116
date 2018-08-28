22353e968158c297abb76c64e8dd7596151321fe
CarlosMorales1171316 <carlosmorales1284@hotmail.com>
2018-08-28 10:01:37 -0600

    Commit
    lab0 commit

--- Laboratorio0/app/src/main/java/com/example/charlie_pc/laboratorio0/MainActivity.java
+++ Laboratorio0/app/src/main/java/com/example/charlie_pc/laboratorio0/MainActivity.java
@@ -27,7 +27,7 @@ import java.util.TreeMap;
 import static com.example.charlie_pc.laboratorio0.R.id.botonBuscar;
 import static com.example.charlie_pc.laboratorio0.R.id.botonBuscar;
 import static com.example.charlie_pc.laboratorio0.R.id.textView1;
-import static com.example.charlie_pc.laboratorio0.R.id.txtNombre;
+import static com.example.charlie_pc.laboratorio0.R.id.txtBuscar;
 import static java.util.Collections.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     List<HashMap<String, String>> listItems3 = new ArrayList<>();
     HashMap<String, String> playlist = new HashMap<String, String>();
 
-    Button buscar, ascendente, descendente;
-    EditText buscarNombre;
+    Button buscar, ascendente, descendente, agregar;
+    EditText buscarNombre, agregarNombre, agregarDuracion;
     ListView mostrarLista;
     TextView txtView;
 
@@ -47,16 +47,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buscarNombre = (EditText) findViewById(R.id.txtNombre);
+        buscarNombre = (EditText) findViewById(R.id.txtBuscar);
+        agregarNombre = (EditText) findViewById(R.id.txtNombreCancion);
+        agregarDuracion = (EditText) findViewById(R.id.txtDuracionCancion);
         buscar = (Button) findViewById(R.id.botonBuscar);
         ascendente = (Button) findViewById(R.id.botonAscendente);
         descendente = (Button) findViewById(R.id.botonDescendente);
+        agregar = (Button) findViewById(R.id.botonAgregar);
         mostrarLista = (ListView) findViewById(R.id.ListView1);
         txtView = (TextView) findViewById(R.id.textView1);
 
         buscar.setOnClickListener(this);
         ascendente.setOnClickListener(this);
         descendente.setOnClickListener(this);
+        agregar.setOnClickListener(this);
 
         playlist.put("Arabella", "3:43");
         playlist.put("Snap out of it", "2:50");
@@ -88,57 +92,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         mostrarLista.setAdapter(adapter);
     }
-
+    String value = "";
         @Override
     public void onClick(View v) {
 
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             //case1 buscar
             case R.id.botonBuscar:
-                listItems.clear();
                 String nombreCancion = buscarNombre.getText().toString();
                 boolean flag = false;
+                if (nombreCancion.isEmpty() == false) {
                     if (playlist.containsKey(nombreCancion)) {
                         flag = true;
+                        value = playlist.get(nombreCancion);
                     } else {
                         flag = false;
                     }
                     buscarNombre.getText().clear();
                     buscarNombre.setText("");
-                if (flag == true)
-                {
-                    txtView.setText(nombreCancion + " SI se encuentra en el Playlist");
-                }
-                else
-                {
-                    txtView.setText("Esta canción NO se encuentra en el Playlist");
+                    if (flag == true) {
+                        txtView.setText(nombreCancion + " | " + value + " | \n" + " Si se encuentra en tu Playlist");
+                    } else {
+                        txtView.setText("No se encuentraron resultados para: \n" + nombreCancion);
+                    }
+                } else {
+                    txtView.setText("Ingrese el nombre de una canción");
                 }
                 break;
 
-                //case2 ordenarAscendente
+            //case2 ordenarAscendente
             case R.id.botonAscendente:
                 listItems2.clear();
-                TreeMap<String,String> treeMap = new TreeMap<String,String>(playlist);
-                for(Map.Entry<String,String>entry:treeMap.entrySet()){}
+                TreeMap<String, String> treeMap = new TreeMap<String, String>(playlist);
+                for (Map.Entry<String, String> entry : treeMap.entrySet()) {
+                }
 
-            SimpleAdapter adaptador = new SimpleAdapter(this, listItems2, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.texto2});
-            Iterator iterador = treeMap.entrySet().iterator();
-            while (iterador.hasNext()) {
-                HashMap<String, String> resultadoMap = new HashMap<>();
-                Map.Entry par = (Map.Entry) iterador.next();
-                resultadoMap.put("First Line", par.getKey().toString());
-                resultadoMap.put("Second Line", par.getValue().toString());
-                listItems2.add(resultadoMap);
-            }
+                SimpleAdapter adaptador = new SimpleAdapter(this, listItems2, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.texto2});
+                Iterator iterador = treeMap.entrySet().iterator();
+                while (iterador.hasNext()) {
+                    HashMap<String, String> resultadoMap = new HashMap<>();
+                    Map.Entry par = (Map.Entry) iterador.next();
+                    resultadoMap.put("First Line", par.getKey().toString());
+                    resultadoMap.put("Second Line", par.getValue().toString());
+                    listItems2.add(resultadoMap);
+                }
                 mostrarLista.setAdapter(adaptador);
                 break;
 
             //case3 ordenarDescendente
             case R.id.botonDescendente:
                 listItems3.clear();
-                TreeMap<String,String> treeMap2 = new TreeMap<String,String>(playlist);
-                for(Map.Entry<String,String>entry:treeMap2.entrySet()){
+                TreeMap<String, String> treeMap2 = new TreeMap<String, String>(playlist);
+                for (Map.Entry<String, String> entry : treeMap2.entrySet()) {
                 }
                 SimpleAdapter adaptador2 = new SimpleAdapter(this, listItems3, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.texto2});
                 Iterator iterador2 = treeMap2.descendingMap().entrySet().iterator();
@@ -150,6 +155,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     listItems3.add(resultadoMap);
                 }
                 mostrarLista.setAdapter(adaptador2);
+                break;
+
+            //case4 Agregar
+            case R.id.botonAgregar:
+                String StringNombreCancion = agregarNombre.getText().toString();
+                String StringDuracionCancion = agregarDuracion.getText().toString();
+
+                if(StringNombreCancion.isEmpty()==false && StringDuracionCancion.isEmpty()==false) {
+                    if (playlist.containsKey(StringNombreCancion) == true) {
+                        txtView.setText(StringNombreCancion + " ya existe en tu Playlist");
+                    } else {
+                        playlist.put(StringNombreCancion, StringDuracionCancion);
+                        txtView.setText(StringNombreCancion + " se ha agregado a tu Playlist");
+                        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.texto2});
+                        Iterator it = playlist.entrySet().iterator();
+                        while (it.hasNext()) {
+                            HashMap<String, String> resultMap = new HashMap<>();
+                            Map.Entry pair = (Map.Entry) it.next();
+                            resultMap.put("First Line", pair.getKey().toString());
+                            resultMap.put("Second Line", pair.getValue().toString());
+                            listItems.add(resultMap);
+                        }
+                        mostrarLista.setAdapter(adapter);
+                    }
+                }
+                else
+                {
+                    txtView.setText("Debe llenar todos los campos");
+                }
                 break;
         }
     }
